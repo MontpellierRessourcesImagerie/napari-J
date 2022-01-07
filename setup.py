cb9d530b1cd65d520b98a4ad243bf7a193698806
volker-baecker <volker.baecker@mri.cnrs.fr>
2022-01-07 14:48:32 +0100

    added info to the setup

--- setup.py
+++ setup.py
@@ -9,9 +9,29 @@ use_scm = {"write_to": "napari_j/_version.py"}
 
 setup(
     use_scm_version=use_scm,
+    
     entry_points={
         'napari.plugin': [
             'naparij = napari_j',
         ],
     },
+    
+    classifiers=[
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Science/Research',
+	'Topic :: Scientific/Engineering',
+	'License :: OSI Approved :: MIT License',
+
+	'Operating System :: OS Independent',
+
+	'Programming Language :: Python :: 3.7',
+	'Programming Language :: Python :: 3.8',
+	'Programming Language :: Python :: 3.9',
+
+	'Framework :: napari',
+    ],
+    
+     install_requires=[
+        'jpype1>=1.2.1',
+    ],
 )
